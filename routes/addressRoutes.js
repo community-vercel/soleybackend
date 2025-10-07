f81aab10ca95c82c6cf021173b9b4dabebd807e9
community-vercel <communityvercel@gmail.com>
2025-10-07 18:36:51 +0500

    'wwsw'

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const addressController = require('../controllers/addressController');
+const addressController = require('../controllers/Addresscontroller');
 
 
 router.get('/', addressController.getSavedAddresses);

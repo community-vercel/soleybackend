4b15612c0c378abe2a8b86b948b33c32bdc389df
community-vercel <communityvercel@gmail.com>
2025-10-07 21:32:16 +0500

    'wwsw'

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -2,15 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const addressController = require('../controllers/Addresscontroller');
+const { auth, authorize } = require('../middleware/auth');
 
 
-router.get('/', addressController.getSavedAddresses);
-router.post('/', addressController.saveAddress);
-router.put('/:addressId', addressController.updateAddress);
-router.delete('/:addressId', addressController.deleteAddress);
-router.patch('/:addressId/default', addressController.setDefaultAddress);
-router.post('/validate', addressController.validateAddress);
-router.get('/autocomplete', addressController.getAddressAutocomplete);
+router.get('/', auth,addressController.getSavedAddresses);
+router.post('/',auth, addressController.saveAddress);
+router.put('/:addressId',auth, addressController.updateAddress);
+router.delete('/:addressId',auth ,addressController.deleteAddress);
+router.patch('/:addressId/default', auth,addressController.setDefaultAddress);
+router.post('/validate',auth, addressController.validateAddress);
+router.get('/autocomplete',auth, addressController.getAddressAutocomplete);
 
-router.get('/place-details', addressController.getPlaceDetails);
+router.get('/place-details',auth, addressController.getPlaceDetails);
 module.exports = router;
